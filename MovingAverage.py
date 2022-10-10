7e1b320ad5829a0de570ff3250a5b6318cd95934
Vettoa <kcharchan@gmail.com>
2022-10-10 19:21:33 +0200

    class SakataChart

--- MovingAverage.py
+++ MovingAverage.py
@@ -8,33 +8,48 @@ with open('E:\Python\cdr.csv', newline='') as csvfile:  #Read csv file
     data_high_low = list(map(lambda x: (x[0], float(x[2]), float(x[3])), spam)) #'Data' with higher prices and lower prices
     data_open_close = list(map(lambda x: (x[0], float(x[1]), float(x[4])), spam)) #'Data' with higher prices and lower prices
 
-def moving_avg(prices, window_size): #Moving average for close prices
-    avg = []
-    for x in range(0, len(prices) - window_size+1):
-        new_prices = prices[x:window_size+x]
-        number = round(sum(new_prices) / window_size, 2)
-        avg.append(number)
-    return avg
+class SakataChart:
 
-def moving_min(prices, window_size): #Moving minimum for close prices
-    avg = []
-    for x in range(0, len(prices) - window_size+1):
-        new_prices = prices[x:window_size+x]
-        number = min(new_prices)
-        avg.append(number)
-    return avg
+    def __init__(self, prices, window_size):
+        self.prices = prices
+        self.window_size = window_size
 
-def single_candle_height(data): #Single candle height
-    return [round(highs - lows,2) for data, highs, lows in data]
+    def moving_avg(self): #Moving average for close prices
+        avg = []
+        for x in range(0, len(self.prices) - self.window_size+1):
+            new_prices = self.prices[x:self.window_size+x]
+            number = round(sum(new_prices) / self.window_size, 2)
+            avg.append(number)
+        return avg
 
-def calculate_support(prices, window_size, ratio = 0.1): #Calculating support for prices
-    minimum_avg = moving_min(prices, window_size)
-    high_low_list = [round(highs - lows,2) for data, highs, lows in data_high_low]
-    supp_avg = [round(values - (diff * ratio), 2) for values, diff in zip(minimum_avg, high_low_list)]
+    def moving_min(self): #Moving minimum for close prices
+        avg = []
+        for x in range(0, len(self.prices) - self.window_size+1):
+            new_prices = self.prices[x:self.window_size+x]
+            number = min(new_prices)
+            avg.append(number)
+        return avg
 
-    return supp_avg
+    def single_candle_height(self, data): #Single candle height
+        '''
+        Data with higher and lowest prices
+        '''
+        return [round(highs - lows,2) for data, highs, lows in data]
 
-def find_doji(data): #Finding doji
-    doji = list(map(lambda x: abs(x[1]- x[2]), data))
-    data_list = list(zip(doji, data))
-    return '{}'.format(min(data_list)[1][0])
+    def calculate_support(self, data, ratio = 0.1): #Calculating support for prices
+        '''
+        Data with higher and lowest prices
+        '''
+        minimum_avg = self.moving_min()
+        high_low_list = [round(highs - lows,2) for data, highs, lows in data]
+        supp_avg = [round(values - (diff * ratio), 2) for values, diff in zip(minimum_avg, high_low_list)]
+
+        return supp_avg
+
+    def find_doji(self, data): #Finding doji
+        '''
+        Data with open and close prices
+        '''
+        doji = list(map(lambda x: abs(x[1]- x[2]), data))
+        data_list = list(zip(doji, data))
+        return '{}'.format(min(data_list)[1][0])
