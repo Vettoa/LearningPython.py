b5ac41385706229bd1ad3e25ba7126c8af7d532a
Vettoa <kcharchan@gmail.com>
2023-01-08 16:38:32 +0100

    def is_empty

--- stack.py
+++ stack.py
@@ -2,8 +2,6 @@ class EmptyStackError(Exception):
     'The stack is Empty'
     pass
 
-
-
 class Stack:
 
     def __init__(self):
@@ -16,7 +14,20 @@ class Stack:
         return self.__data.insert(0, item) #Adding item to the stack list on the first postiion
 
     def pop(self): #Showing first item from stack
-        if len(self.__data) == 0:
+        if not self.is_empty():
             raise EmptyStackError('The stack is Empty')
         else:
             return self.__data.pop(0)
+
+    def is_empty(self): #Checking if the list is empty
+        return self.__len__() > 0
+
+techs = Stack()
+techs.push('python')
+techs.push('python1')
+techs.push('python2')
+techs.pop()
+techs.pop()
+techs.pop()
+
+print(techs.is_empty())
